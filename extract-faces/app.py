93b92d65f6708895e591728ccc512cab1d1168d4
Guilherme Joviniano de Sousa <00drpixelss@gmail.com>
2023-02-22 13:17:22 -0300

    add

--- extract-faces/app.py
+++ extract-faces/app.py
@@ -1,10 +1,12 @@
 from retinaface import RetinaFace
+from deepface import DeepFace
 from datetime import datetime
 import cv2
 import uuid
 import time
 import pandas as pd
 import os 
+import shutil
 
 list_of_cams = pd.read_csv("CAMS.csv", sep=',')['IPv4 Address']
 PASSWORD = ''
@@ -55,13 +57,38 @@ def detect_and_save_faces(image_path, ip):
         print('NO FACES DETECTED!')
         return False
 
+def not_in_db():
+    not_in_db = []
+    for image in os.listdir('./tmp'):
+        print(image)
+        try:
+            df = DeepFace.find(img_path = './tmp/' + image, 
+                db_path = "../database", 
+                detector_backend = backends[4],
+                enforce_detection = False
+            )
+            try: 
+                df.head()
+            except: 
+                not_in_db.append(image)
+        except: 
+            continue
+    return not_in_db
 
 get_save_image_of_cams(list_of_cams)
+
 for image in os.listdir('./cam/tmp/'):
     ip = image.split('-')[1].split('png')[0][:-1]
     detect_and_save_faces('./cam/tmp/' + image, ip)
 
 # TODO check in db and save!
+files = not_in_db()
+
+if len(files) > 0: 
+    for file in files:
+        shutil.move('./tmp/' + file, '../database/' + file)
+    os.remove('../database/representations_vgg_face.pkl')
+    
 
 # TODO run every 30 seconds, and clean tmp folders
 
